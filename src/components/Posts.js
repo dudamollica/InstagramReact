3ea12f9699a62b22d7a96632bed6d7b8b1aa1821
dudamollica <dudarenno@yahoo.com.br>
2022-11-20 20:52:12 -0300

    Dados dinamicos:nome,imagem e salvar-implementados

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,52 +1,63 @@
+import React from "react"
+
 export default function Posts() {
-    const infosPosts=[
-        {imagemUsuario:"assets/img/meowed.svg", nomeUsuario:"meowed", imagemConteudo:"assets/img/gato-telefone.svg", imgQuemCurtiu:"assets/img/respondeai.svg", quemCurtiu:"respondeai", quantasCurtidas:"101.523"},
-        {imagemUsuario:"assets/img/barked.svg", nomeUsuario:"barked", imagemConteudo:"assets/img/dog.svg", imgQuemCurtiu:"assets/img/adorable_animals.svg", quemCurtiu:"adorable_animals", quantasCurtidas:"99.159"}
+    const infosPosts = [
+        { imagemUsuario: "assets/img/meowed.svg", nomeUsuario: "meowed", imagemConteudo: "assets/img/gato-telefone.svg", imgQuemCurtiu: "assets/img/respondeai.svg", quemCurtiu: "respondeai", quantasCurtidas: "101.523" },
+        { imagemUsuario: "assets/img/barked.svg", nomeUsuario: "barked", imagemConteudo: "assets/img/dog.svg", imgQuemCurtiu: "assets/img/adorable_animals.svg", quemCurtiu: "adorable_animals", quantasCurtidas: "99.159" }
     ]
-        return (
-            <div class="posts">
-                {infosPosts.map((p)=> <UmPost imagemUsuario={p.imagemUsuario} nomeUsuario={p.nomeUsuario} imagemConteudo={p.imagemConteudo} imgQuemCurtiu={p.imgQuemCurtiu} quemCurtiu={p.quemCurtiu} quantasCurtidas={p.quantasCurtidas} />)}
-    
-            </div>
-        )
+    return (
+        <div class="posts">
+            {infosPosts.map((p) => <UmPost imagemUsuario={p.imagemUsuario} nomeUsuario={p.nomeUsuario} imagemConteudo={p.imagemConteudo} imgQuemCurtiu={p.imgQuemCurtiu} quemCurtiu={p.quemCurtiu} quantasCurtidas={p.quantasCurtidas} />)}
+
+        </div>
+    )
+}
+
+function UmPost(props) {
+    const [salvar, setSalvar] = React.useState(<ion-icon name="bookmark-outline"></ion-icon>)
+
+    function salvarOuNao() {
+        if (salvar.props.name == 'bookmark-outline') {
+            setSalvar(<ion-icon name="bookmark"></ion-icon>)
+        } else {
+            setSalvar(<ion-icon name="bookmark-outline"></ion-icon>)
+        }
     }
-    
-    function UmPost(props){
-       return (
+    return (
         <div class="post">
-                    <div class="topo">
-                        <div class="usuario">
-                            <img src={props.imagemUsuario} />
-                            {props.nomeUsuario}
-                        </div>
-                        <div class="acoes">
-                            <ion-icon name="ellipsis-horizontal"></ion-icon>
-                        </div>
+            <div class="topo">
+                <div class="usuario">
+                    <img src={props.imagemUsuario} />
+                    {props.nomeUsuario}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+
+            <div class="conteudo">
+                <img src={props.imagemConteudo} />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
-    
-                    <div class="conteudo">
-                        <img src={props.imagemConteudo} />
-                    </div>
-    
-                    <div class="fundo">
-                        <div class="acoes">
-                            <div>
-                                <ion-icon name="heart-outline"></ion-icon>
-                                <ion-icon name="chatbubble-outline"></ion-icon>
-                                <ion-icon name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div>
-                                <ion-icon name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-    
-                        <div class="curtidas">
-                            <img src={props.imgQuemCurtiu} />
-                            <div class="texto">
-                                Curtido por <strong>{props.quemCurtiu}</strong> e <strong>outras {props.quantasCurtidas} pessoas</strong>
-                            </div>
-                        </div>
+                    <div onClick={salvarOuNao}>
+                        {salvar}
                     </div>
                 </div>
-       )
-    }
+
+                <div class="curtidas">
+                    <img src={props.imgQuemCurtiu} />
+                    <div class="texto">
+                        Curtido por <strong>{props.quemCurtiu}</strong> e <strong>outras {props.quantasCurtidas} pessoas</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,21 +1,27 @@
+import React from "react"
+
 export default function Usuario() {
 
     return (
-       <UmUsuario imagem="assets/img/catanacomics.svg" usuario="catanacomics" nome="Catana"/>
+        <UmUsuario imagem="assets/img/catanacomics.svg" usuario="catanacomics" nome="Catana" />
     )
 }
 
-function UmUsuario(props){
-    return(
-        <div class="usuario">
-        <img src={props.imagem} />
-        <div class="texto">
-            <strong>{props.usuario}</strong>
-            <span>
-                {props.nome}
-                <ion-icon name="pencil"></ion-icon>
-            </span>
+function UmUsuario(props) {
+    const [nome, setNome] = React.useState("")
+    const [imagem, setImagem] = React.useState("")
+
+    return (
+        <div className="usuario">
+            <img src={!imagem ? (props.imagem) : (imagem)} onClick={()=>setImagem(prompt("Coloque a URL da sua foto"))}/>
+            <div className="texto">
+                <strong>{props.usuario}</strong>
+                <span>
+                    {!nome ? (props.nome) : (nome)}
+                    <ion-icon name="pencil" onClick={()=>setNome(prompt("Qual seu nome?"))}></ion-icon>
+                </span>
+            </div>
         </div>
-    </div>
     )
 }
+
